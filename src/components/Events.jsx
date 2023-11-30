f157742948b57f4f5c509d83328df0ee90a2b46a
pratik-prajn <pratikprajn007@gmail.com>
2023-11-30 22:09:25 +0530

    vvv

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -18,7 +18,7 @@ const Events = () => {
 
         <div className='ind-section'>
                 <div className="sub-ind-sec1">
-                    <div className='ind-head'> Recent Events   </div>
+                   
 
 
 
@@ -27,26 +27,27 @@ const Events = () => {
                     <Table>
             <thead>
               <tr>
-                <th>INSTITUTE</th>
-                <th>COURSE</th>
-                <th>YEAR OF PASSING</th>
+                <th>DATE</th>
+                
+                <th>EVENT</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>IIT Kharagpur</td>
-                <td>Ph.D. </td>
-                <td>2016</td>
+                <td>14/12/2022</td>
+         
+                <td>"International Conference on Frontiers in Materials Engineering" December 14 – 16, 2022 Organized by : Department of Metallurgy Engineering and Materials Science (MEMS) Venue: Indian Institute of Technology Indore Campus 
+                    <a href="http://mems.iiti.ac.in/docs/Abstract%20Book_ICFME-2022.pdf">Details Abstract Book of ICFME-2022</a></td>
               </tr>
               <tr>
-                <td>IIT Kharagpur</td>
-                <td>M.Tech </td>
-                <td>2011</td>
+                <td>14/12/2022</td>
+              
+                <td>3-Days Active Learning Online Faculty Development Course on Advanced Technology For Integrated Computational Materials Engineering Education <a href="http://mems.iiti.ac.in/docs/TEQIP_ICME_December2020.pdf">Details</a></td>
               </tr>
               <tr>
-                <td>BESU Shibpur</td>
-                <td>B.E </td>
-                <td>2009</td>
+                <td>14/12/2022</td>
+              
+                <td>Dr. Rupesh S. Devan is serving as an Editor for the Chinese J of Physics; Elsevier <a href="https://www.sciencedirect.com/journal/chinese-journal-of-physics/about/editorial-board">Details</a></td>
               </tr>
             </tbody>
           </Table>
@@ -69,14 +70,7 @@ const Events = () => {
 
 
              
-                    <span>&#8226;</span> "International Conference on Frontiers in Materials Engineering" December 14 – 16, 2022 Organized by : Department of Metallurgy Engineering and Materials Science (MEMS) Venue: Indian Institute of Technology Indore Campus 
-                    <a href="http://mems.iiti.ac.in/docs/Abstract%20Book_ICFME-2022.pdf">Details Abstract Book of ICFME-2022</a> 
-<br />
-                    <br /><span>&#8226;</span> 3-Days Active Learning Online Faculty Development Course on Advanced Technology For Integrated Computational Materials Engineering Education <a href="http://mems.iiti.ac.in/docs/TEQIP_ICME_December2020.pdf">Details</a>
-                    <br />
-                    <br /><span>&#8226;</span> Dr. Rupesh S. Devan is serving as an Editor for the Chinese J of Physics; Elsevier <a href="https://www.sciencedirect.com/journal/chinese-journal-of-physics/about/editorial-board">Details</a>
-                    <br />
-                    <br /><span>&#8226;</span> Dr.Mrigendra Dubey has received the Young scientist award for the year 2017 from International academy of Physical sciences
+                    
                 </div>
                 {/* <div className="sub-ind-sec2">
                 <div className='ind-head'> BROCHURE</div>
