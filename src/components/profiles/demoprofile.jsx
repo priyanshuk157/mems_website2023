f89f24f379b119b7dccaf700820cd5bc6611660f
priyanshu kumbhare <mems210005035@iiti.ac.in>
2023-12-05 07:59:04 +0530

    done

--- src/components/profiles/demoprofile.jsx
+++ src/components/profiles/demoprofile.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Navbar from "../navbar";
 import Footer from "../footer";
 import "./demo.css";
-import { FaAngleLeft } from "react-icons/fa";
-import { FaEnvelope, FaHome, FaLinkedin, FaPhone, FaResearchgate,  FaTwitterSquare } from "react-icons/fa";
+// import { FaAngleLeft } from "react-icons/fa";
+import { FaEnvelope,  FaPhone, FaResearchgate } from "react-icons/fa";
 import Table from "react-bootstrap/Table";
 import {SiGooglescholar } from "react-icons/si";
 import prof44 from "./profimages/ghosh.jpg"
