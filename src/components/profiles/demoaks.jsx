ffc1c840daaac616842d72d3364496b5ba39ee8e
pratik-prajn <pratikprajn007@gmail.com>
2023-12-07 02:08:03 +0530

    vv

--- src/components/profiles/demoaks.jsx
+++ src/components/profiles/demoaks.jsx
@@ -6,7 +6,7 @@ import "./demo.css";
 import { FaEnvelope,  FaPhone, FaResearchgate } from "react-icons/fa";
 import Table from "react-bootstrap/Table";
 import {SiGooglescholar } from "react-icons/si";
-import prof44 from "./profimages/ghosh.jpg"
+import prof44 from "./profimages/AKKsir.jpeg"
 
 const profile11 = () => {
   return (
