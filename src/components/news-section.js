0fd693832761460674ef744b17bd2bbe82ef80c8
pratik-prajn <pratikprajn007@gmail.com>
2023-12-08 12:09:48 +0530

    ss

--- src/components/news-section.js
+++ src/components/news-section.js
@@ -10,7 +10,11 @@ import isro from "./images/isro.jpg"
 import k from "./images/kusenter.jpg"
 import r from "./images/ranenter.jpg"
 
-
+import ira from "./images/irina.jpg"
+import nk from "./images/nk.jpg"
+import af from "./images/af.jpg"
+import aj from "./images/ajay.jpg"
+import hema from "./images/heman.jpg"
 import "./news.css";
 const Newssection = () => {
   return (
@@ -44,7 +48,32 @@ const Newssection = () => {
 She pursued her PhD from IIT Kanpur and later worked as a Post-Doc at Max-Planck-Institut für Eisenforschung GmbH, Germany</div> </SwiperSlide>
         <SwiperSlide><div className='swiper-cont'> <img src={r} alt='oky' /> Department of MEMS welcomes Dr. Ranjith Kumar Poobalan. He Joined the Department on 10th October as Assistant Professor.
 </div></SwiperSlide>
-      
+
+
+<SwiperSlide><div className='swiper-cont'> <img src={ira} alt='oky' /> Prof. Irina Alexandrovna Kurzina delivered an expert
+lecture titled "Bimetallic catalysts based on palladium
+for selective oxidation".</div></SwiperSlide>
+        <SwiperSlide><div className='swiper-cont'>  <img src={af} alt='oky' /> Prof. Ahoutou Paul KOUAKOU joined us as visiting
+scientist under CV Raman International Fellowship
+for 3 months.</div> </SwiperSlide>
+        <SwiperSlide><div className='swiper-cont'> <img src={nk} alt='oky' /> Professor N.K. Mukhopadhyay delivered an expert
+lecture titled 'Multicomponent High Entropy Alloys:
+Challenges and Future Prospects'. He also
+interacted with the Department faculty and discussed
+the future research direction
+</div></SwiperSlide>
+<SwiperSlide><div className='swiper-cont'>  <img src={aj} alt='oky' /> Dr. Ajay K Kushwaha visited INST Mohali as an
+external Examiner for Ph.D thesis viva. He had a
+fruitful meeting with Prof. Abir De Sarkar , Dr.
+Ramendra Dey, Dr. Dipankar Mandal, Dr. Rehan
+Khan, Dr. Amit and Dr. Himanshu during his visit.</div> </SwiperSlide>
+        <SwiperSlide><div className='swiper-cont'> <img src={hema} alt='oky' /> Dr. Hemant Borkar Participated in 29th International
+Conference on Processing and Fabrication of
+Advanced Materials (PFAM 2023) organized by IIT
+Tirupati and under his supervision Mr. Hitesh Patil
+(Research Scholar, IIT Indore) won third prize in
+Best Oral Presentation.
+</div></SwiperSlide>
         {/* <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
         <SwiperSlide>Slide 6</SwiperSlide>
