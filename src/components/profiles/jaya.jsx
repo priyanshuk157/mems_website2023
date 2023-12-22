9b178ce645f67fd7d9da63fcea4e7a7e4f14f554
priyanshu kumbhare <mems210005035@iiti.ac.in>
2023-12-22 08:08:36 +0530

    new

--- src/components/profiles/jaya.jsx
+++ src/components/profiles/jaya.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import "./demo.css";
 // import { FaAngleLeft } from "react-icons/fa";
 import { FaEnvelope,  FaPhone, FaResearchgate } from "react-icons/fa";
-import Table from "react-bootstrap/Table";
 import {SiGooglescholar } from "react-icons/si";
 import prof44 from "./profimages/muru.jpg"
 
