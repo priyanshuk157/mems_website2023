2e4e787fd007e6e3a8fa7f2fc14f340c1446562f
pratik-prajn <pratikprajn007@gmail.com>
2023-12-29 17:39:52 +0530

    slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,7 +5,7 @@ import jai2 from "./images/statimg2.jpg"
 
 import newimage from "./images/WhatsApp Image 2023-12-27 at 2.30.02 PM.jpeg"
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import React, { useState } from 'react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
@@ -13,30 +13,39 @@ import 'swiper/css/pagination';
 import "swiper/css/navigation";
 
 // import required modules
-import { Autoplay , Pagination} from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
+
 
 function CarouselFadeExample() {
+  const [swiperRef, setSwiperRef] = useState(null);
   
   return (
     <>
     <div className='carousel-main'>
-    <Swiper
-    
+    {/*<Swiper
+    style={{
+      "--swiper-navigation-color": "#000",
+      "--swiper-navigation-size": "25px",
+      "--swiper-navigation-top-offset": "50%",
+      "--swiper-navigation-sides-offset": "0px",
+     
+    }} 
         slidesPerView={3}
         spaceBetween={0}
         // freeMode={true}
         loop={true}
-        preventInteractionOnTransition={true}
+        preventInteractionOnTransition={false}
         autoplay={{
           delay: 1,
           disableOnInteraction: false,
         }}
-        speed={5000}
+        speed={10000}
         freemodemomentum= "false"
         pagination={{
-          clickable: false,
+          clickable: true,
         }}
-        modules={[Autoplay, Pagination]}
+        navigation={true}
+        modules={[Autoplay, Pagination,Navigation]}
         className="homepage-slider"
       >
         <SwiperSlide><div className="img-hgt"><img src={jai} alt="" /></div></SwiperSlide>
@@ -45,7 +54,27 @@ function CarouselFadeExample() {
         <SwiperSlide><div className="img-hgt"><img src={jai} alt="" /></div></SwiperSlide>
         <SwiperSlide><div className="img-hgt"><img src={newimage} alt="" /></div></SwiperSlide>
         
+      </Swiper>*/}
+      <Swiper
+        onSwiper={setSwiperRef}
+        slidesPerView={3}
+        centeredSlides={true}
+        loop={true}
+        spaceBetween={30}
+        pagination={{
+          type: 'fraction',
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><div className="img-hgt"><img src={jai} alt="" /></div></SwiperSlide>
+        <SwiperSlide><div className="img-hgt"><img src={jai1} alt="" /></div></SwiperSlide>
+        <SwiperSlide><div className="img-hgt"><img src={jai2} alt="" /></div></SwiperSlide>
+        <SwiperSlide><div className="img-hgt"><img src={jai} alt="" /></div></SwiperSlide>
+        <SwiperSlide><div className="img-hgt"><img src={newimage} alt="" /></div></SwiperSlide>
       </Swiper>
+  
    
     </div>
     </>
