db62f45963ee723e143e031a0e9b443e953ad7ae
pratik-prajn <pratikprajn007@gmail.com>
2024-03-12 13:00:09 +0530

    df

--- src/components/profiles/dkr.jsx
+++ src/components/profiles/dkr.jsx
@@ -39,12 +39,30 @@ const profile11 = () => {
               <li>Office		: Room No. 1D- 418 </li>
               <li>Email	 (<i><FaEnvelope/></i>)	: dkrai@iiti.ac.in </li>
               <li>Contact  (<i><FaPhone/></i>)		: +91-731660-3278 </li>
+
+              <div className="homepage_div" >
+              <span className="homepage" >Home Page</span>
+            
+              <a href="http://people.iiti.ac.in/~dkr/ " target="_blank" rel="noreferrer"> <i><FaHome/></i></a></div>
+              <div className="homepage_div" >
+              <span className="homepage" >Google Scholor</span>
+            
+              <a href="  https://scholar.google.com/citations?user=pFwwR8UAAAAJ  "target="_blank"  rel="noreferrer"> <i><SiGooglescholar/></i></a></div>
+              <div className="homepage_div" >
+              <span className="homepage" >Research Gate</span>
+            
+              <a href="https://www.researchgate.net/profile/Dhirendra-Rai"target="_blank"  rel="noreferrer"><i><FaResearchgate/></i></a></div>
+              <div className="homepage_div" >
+              <span className="homepage" >Twitter</span>
+            
+              <a href="@Dhirend59162402"target="_blank"  rel="noreferrer"><i><FaTwitterSquare/></i></a></div>
+
             <div className="fac-links">
              
-            <a href="http://people.iiti.ac.in/~dkr/ " target="_blank" rel="noreferrer"> <i><FaHome/></i></a>
-             <a href="  https://scholar.google.com/citations?user=pFwwR8UAAAAJ  "target="_blank"  rel="noreferrer"> <i><SiGooglescholar/></i></a>
-             <a href="https://www.researchgate.net/profile/Dhirendra-Rai"target="_blank"  rel="noreferrer"><i><FaResearchgate/></i></a>
-             <a href="@Dhirend59162402"target="_blank"  rel="noreferrer"><i><FaTwitterSquare/></i></a>
+            
+             
+             
+             
              {/* <a href="https://in.linkedin.com/in/dr-ajay-k-kushwaha-16b46a49?trk=public_post_feed-actor-name "target="_blank"  rel="noreferrer"> <i><FaLinkedin/></i></a> */}
               
               
