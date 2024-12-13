b65bd0956117a87a4af299168e6cd39103a33ac4
pratik-prajn <pratikprajn007@gmail.com>
2024-12-13 18:50:07 +0530

    fff

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,66 +1,79 @@
-import React from 'react'
-
 import { Accordion } from 'react-bootstrap';
-import "./career.css"
+import "./career.css";
+
 const Career = () => {
   return (
     <>
-   
+      <span className="heading">CAREERS</span>
 
-<span className="heading">CAREERS</span>
+      <div className="career-cont">
+        <Accordion className='career-accord' defaultActiveKey="-1">
 
-<div className="career-cont">
-<Accordion className='career-accord' defaultActiveKey="-1">
+          <Accordion.Item eventKey="4">
+            <Accordion.Header><span className='res-header'>Faculty Positions</span></Accordion.Header>
+            <Accordion.Body>
+              We are hiring <a href="https://www.iiti.ac.in/recruitments/faculty-positions">click here</a>
+            </Accordion.Body>
+          </Accordion.Item>
 
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>
+              <span className='res-header'>Project/Research Positions & JRF position</span>
+            </Accordion.Header>
+            <Accordion.Body>
+              <h4 style={{ margin: "0px", color: "#c44536", marginTop: "15px" }}>Project Title:</h4>
+              <p>
+                Characterization of Idols, Coins, Ornaments, and Other Metallic Monuments from Ancient and Medieval Times in the Malwa Region Using In-situ and Ex-situ Experimental Techniques
+              </p>
 
-<Accordion.Item eventKey="4">
- <Accordion.Header><span className='res-header'>Faculty Positions</span></Accordion.Header>
- <Accordion.Body>
-We  are  hiring <a href=" https://www.iiti.ac.in/recruitments/faculty-positions">click  here</a>
+              <h4 style={{ margin: "0px", color: "#c44536", marginTop: "15px" }}>Principal Investigators:</h4>
+              <ul>
+                <li>Prof. Eswar Prasad Korimilli</li>
+                <li>Prof. K.V Vamsi</li>
+              </ul>
 
- </Accordion.Body>
-</Accordion.Item>
+              <h4 style={{ margin: "0px", color: "#c44536", marginTop: "15px" }}>Sponsoring Agency:</h4>
+              <p>Department of Science and Technology (DST), India</p>
 
-<Accordion.Item eventKey="3">
- <Accordion.Header><span className='res-header'>Project/Research Positions & JRF position</span></Accordion.Header>
- <Accordion.Body>
-Watch  out for this space
- </Accordion.Body>
-</Accordion.Item>
+              <h4 style={{ margin: "0px", color: "#c44536", marginTop: "15px" }}>Project Overview:</h4>
+              <p>
+                This project aims to explore the rich cultural heritage of the Malwa region through the scientific characterization of ancient and medieval metallic artifacts. Using advanced in-situ and ex-situ experimental techniques, this research seeks to unveil the material properties, composition, and historical significance of these monuments and artifacts.
+              </p>
 
+              <p>
+                For further details and the application process, click below:
+              </p>
+              <a 
+                href="https://www.iiti.ac.in/recruitments/jrf-srf-positions" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                style={{ color: "#005a9c", textDecoration: "underline" }}
+              >
+                Link to Advertisement
+              </a>
+            </Accordion.Body>
+          </Accordion.Item>
 
-<Accordion.Item eventKey="2">
- <Accordion.Header><span className='res-header'>Staff</span></Accordion.Header>
- <Accordion.Body>
- No position open now
- </Accordion.Body>
-</Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header><span className='res-header'>Staff</span></Accordion.Header>
+            <Accordion.Body>
+              No position open now
+            </Accordion.Body>
+          </Accordion.Item>
 
+          <Accordion.Item eventKey="0">
+            <Accordion.Header><span className='res-header'>Internships</span></Accordion.Header>
+            <Accordion.Body>
+              Watch out for this space
+            </Accordion.Body>
+          </Accordion.Item>
 
+        </Accordion>
 
-
-
-
-
-<Accordion.Item eventKey="0">
- <Accordion.Header > <span className='res-header'>Internships</span> </Accordion.Header>
- <Accordion.Body>
- Watch out for this space
- </Accordion.Body>
-</Accordion.Item>
-
-
-
-</Accordion>
-
-<div className='foot-hgt-adjst'></div>
-
-</div>
-
-   
-   
+        <div className='foot-hgt-adjst'></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Career
+export default Career;
--- src/components/Facilities.jsx
+++ src/components/Facilities.jsx
@@ -12,101 +12,129 @@ import thremoanalyser from "./images/Thermo_analyser.jpg";
 import uvspectro from "./images/UVspectrometer.jpg";
 import xraydiff from "./images/Xraydiff.jpg";
 import srd from "./images/srd.jpg";
+import utm from "./images/UTM1.jpg";
 import "./Facilities.css";
-// import Accordion from 'react-bootstrap/Accordion';
 
 const Facilities = () => {
+  const button = (
+    <a href="https://forms.gle/N7EkoaqFQoRL485H7" target="_blank" rel="noopener noreferrer">
+      <button
+        style={{
+          backgroundColor: "#005a9c",
+          color: "white",
+          padding: "5px 10px",
+          border: "none",
+          borderRadius: "8px",
+          cursor: "pointer",
+          marginTop: "3px",
+        }}
+      >
+        Use This
+      </button>
+    </a>
+  );
+
   return (
     <>
       <div>
-        
-          {" "}
-          <br />
-          <span className="Instruments_In_Use">
-          <h1>Instruments In Use</h1>{" "}</span>
-          <br />
-        
+        <br />
+        <span className="Instruments_In_Use">
+          <h1>Instruments In Use</h1>{" "}
+        </span>
+        <br />
+
         <div className="facilities-items">
-         <div className="facility-images">
-            {" "}
-            <img src={dma} alt="" />
-            DMA{" "}
+          <div className="facility-images">
+            <img src={dma} alt="" /> DMA
             <br />Location: MOM Lab
+            <br />
+            {button}
           </div>
-          
+
           <div className="facility-images">
             <img src={creeptest} alt="" /> Creep Testing Machine
             <br />Location: MOM Lab
+            <br />
+            {button}
           </div>
+
           <div className="facility-images">
             <img src={electrochemical} alt="" /> Electerochemical Work Station
             <br />Location: -
-          </div> 
-          <div className="facility-images">
-            {" "}
-            <img src={fluoroscence} alt="" />
-            Fluorescence
-            <br />Location:-
+            <br />
+            {button}
           </div>
+
           <div className="facility-images">
-            {" "}
-            <img src={fretting} alt="" />
-            Fretting Wear Machine
-            <br />Location:-
+            <img src={fluoroscence} alt="" /> Fluorescence
+            <br />Location: -
+            <br />
+            {button}
           </div>
+
           <div className="facility-images">
-            {" "}
-            <img src={invertoptical} alt="" />
-            Invert Optical Microscope
+            <img src={fretting} alt="" /> Fretting Wear Machine
+            <br />Location: -
+            <br />
+            {button}
+          </div>
+
+          <div className="facility-images">
+            <img src={invertoptical} alt="" /> Invert Optical Microscope
             <br />Location: MOM Lab
+            <br />
+            {button}
           </div>
+
           <div className="facility-images">
-            {" "}
-            <img src={solarcell} alt="" />
-            Solar Cell Simulator
-            <br />Location:-
+            <img src={solarcell} alt="" /> Solar Cell Simulator
+            <br />Location: -
+            <br />
+            {button}
           </div>
+
           <div className="facility-images">
-            {" "}
             <img src={hopkinson} alt="" /> SpliHopkinson Pressure Bar
             <br />Location: MOM Lab
+            <br />
+            {button}
           </div>
+
           <div className="facility-images">
-            {" "}
-            <img src={tempcontrolledutm} alt="" />
-            Temperature Controlled UTM
+            <img src={utm} alt="" /> Temperature Controlled UTM
             <br />Location: MOM Lab
+            <br />
+            {button}
           </div>
+
           <div className="facility-images">
-            {" "}
             <img src={thremoanalyser} alt="" /> Thermo Analyser
             <br />Location: MOM Lab
-          </div>
-          <div className="facility-images">
-            {" "}
-            <img src={uvspectro} alt="" />
-            UV Spectrometer
-            <br />Location:-
-          </div>
-          <div className="facility-images">
-            {" "}
-            <img src={xraydiff} alt="" />
-            X-ray diff
-            <br />Location:-
+            <br />
+            {button}
           </div>
 
           <div className="facility-images">
-            {" "}
-            <img src={srd} alt="" />
-                SRD Machine
+            <img src={uvspectro} alt="" /> UV Spectrometer
+            <br />Location: -
+            <br />
+            {button}
+          </div>
+
+          <div className="facility-images">
+            <img src={xraydiff} alt="" /> X-ray diff
+            <br />Location: -
+            <br />
+            {button}
+          </div>
+
+          <div className="facility-images">
+            <img src={srd} alt="" /> SRD Machine
             <br />Location: MOM Lab
+            <br />
+            {button}
           </div>
-
-
-
         </div>
-
-        
       </div>
     </>
   );
